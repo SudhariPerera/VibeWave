85b55ee90840680fa43c863f13e2511028fe7c6f
Sudhariperera <Sudhari08sudh@gmail.com>
2026-04-08 17:07:09 +1200

    Concert Page 1

---
+++ VibeWave/Controllers/ConcertController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VibeWave.Data;
+using VibeWave.Models;
+
+namespace VibeWave.Controllers
+{
+    public class ConcertController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ConcertController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Concert> objConcertList = _db.Concert.ToList();
+            return View(objConcertList);
+        }
+
+     
+        public IActionResult Create( )
+        {
+             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Concert obj)
+        {
+            if (obj.ConcertName == obj.ConcertCategory.ToString())
+            {
+                ModelState.AddModelError("Concert Name", "The Concert Category cannot exactly match with the Concert Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Concert.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = " Concert Details Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        //EDIT BUTTON
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Concert? concertFromDb = _db.Concert.Find(id);
+            if (concertFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(concertFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Concert obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Concert.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = " Concert Details Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //EDIT BUTTON
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Concert? concertFromDb = _db.Concert.Find(id);
+            if (concertFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(concertFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Concert? obj=_db.Concert.Find(id); 
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            _db.Concert.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = " Concert Details Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VibeWave/Migrations/20260408025443_SeedConcertTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VibeWave.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedConcertTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Concert",
+                columns: new[] { "Id", "ConcertCategory", "ConcertName", "DisplayOrder" },
+                values: new object[,]
+                {
+                    { 1, "More Music", "Come Together - Born to Run - Bruce Springsteen", 1 },
+                    { 2, "Comedy", "Kyla Cobbler - Not My Lemons", 2 },
+                    { 3, "Rock and Pop", "Nurse Georgie Carroll - Infectious", 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
