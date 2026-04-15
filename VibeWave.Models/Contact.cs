b1fe4c080b00b4162ab3472d6fe6da66c078ea7b
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-04-15 14:57:41 +1200

    complete the fundamental function of contact us, and next I will add the function of database saving.

--- VibeWave.Models/Contact.cs
+++ VibeWave.Models/Contact.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VibeWave.Models;
 
 namespace VibeWave.Models
 {
     public class Contact
     {
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        [Display(Name = "Email")]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Display(Name ="Country")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        [Display(Name = "Subject")]
+        [StringLength(500)]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        [Display(Name = "Message")]
+        [StringLength(2000)]
+        public string Message { get; set; }
     }
 }
