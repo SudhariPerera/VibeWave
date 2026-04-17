bc60fc21209a1e27f67a9f7970f3cdf8aa7ef632
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-04-17 13:50:09 +1200

    Add search and filter to Homepage

---
+++ VibeWave.Models/HomeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace VibeWave.Models
+{
+    public class HomeIndexViewModel
+    {
+        public string SearchString { get; set; }
+        public int? CategoryId { get; set; }// you can choose to fill it or not.
+        public List<Concert> Concerts{ get; set; }//one(actor) to many(concert)
+
+    }
+}
--- VibeWave/Areas/Customer/Controllers/HomeController.cs
+++ VibeWave/Areas/Customer/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 using VibeWave.DataAccess.Repository;
 using VibeWave.DataAccess.Repository.IRepository;
 using VibeWave.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VibeWave.Areas.Customer.Controllers
 {
@@ -18,16 +21,56 @@ namespace VibeWave.Areas.Customer.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string searchString, int? categoryId)
         {
             List<Concert> objConcertList = _unitOfWork.Concert.GetAll().ToList();
-            return View(objConcertList);
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                string keyword = searchString.ToLower();//can search in normal and capital words.
+                List<Concert> filteredList = new List<Concert>();//must have
+                foreach (var concert in objConcertList)//to see onhttps://blog.csdn.net/William_cl/article/details/156279181
+                {
+                    if (concert.ConcertName.ToLower().Contains(keyword) || (concert.ActorName != null && concert.ActorName.ToLower().Contains(keyword)))
+                    {
+                        filteredList.Add(concert);
+                    }
+                }
+                objConcertList = filteredList;
+            }
+            if (categoryId.HasValue && categoryId.Value > 0)
+            {
+                List<Concert> filteredList = new List<Concert>();
+                foreach (var concert in objConcertList)
+                {
+                    if (concert.CategoryId == categoryId.Value)
+                    {
+                        filteredList.Add(concert);
+                    }
+                }
+                objConcertList = filteredList;
+            }
+            //like ProductController in Bulkyweb
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.CategoryId.ToString()
+            });
+            ViewBag.CategoryList = CategoryList;
+            objConcertList = objConcertList.OrderBy(c => c.DisplayDate).ToList();
+            var viewModel = new HomeIndexViewModel
+            {
+                SearchString = searchString,
+                CategoryId = categoryId,
+                Concerts = objConcertList
+            };
+            return View(viewModel);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
+        //        public IActionResult Privacy()
+        //        {
+        //            return View();
+        //        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
