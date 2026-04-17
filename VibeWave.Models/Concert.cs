247d9b2f5fc8b29970ae07e79011a77c27d2b662
Sudhariperera <Sudhari08sudh@gmail.com>
2026-04-17 12:56:36 +1200

    Fix some problem in Concert page

--- VibeWave.Models/Concert.cs
+++ VibeWave.Models/Concert.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using VibeWave.Models;
 
 namespace VibeWave.Models
 {
