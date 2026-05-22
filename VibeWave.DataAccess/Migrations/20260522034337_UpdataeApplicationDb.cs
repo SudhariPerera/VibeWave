2b056f83e422c333f9b94adf762fa608765ad2b9
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-22 15:44:02 +1200

    Updated the applicationDb

---
+++ VibeWave.DataAccess/Migrations/20260522034337_UpdataeApplicationDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VibeWave.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdataeApplicationDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DisplayDate",
+                value: new DateOnly(2026, 6, 10));
+
+            migrationBuilder.UpdateData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DisplayDate",
+                value: new DateOnly(2026, 6, 12));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DisplayDate",
+                value: new DateOnly(2026, 5, 1));
+
+            migrationBuilder.UpdateData(
+                table: "Concert",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DisplayDate",
+                value: new DateOnly(2026, 5, 1));
+        }
+    }
+}
