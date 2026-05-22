62cc0395de39e1b2f9eadb1c950af2ebb80dd3f6
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-22 14:19:05 +1200

    Updated calendarVM

---
+++ VibeWave.Models/ViewModels/CalendarVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeWave.Models.ViewModels
+{
+    public class CalendarVM
+    {
+        public string SearchString { get; set; }
+        public int? CategoryId { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public List<Concert> Concerts { get; set; }
+    }
+}
