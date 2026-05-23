d67379f3573966f511115fddb38889abd8974f48
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-05-23 20:09:43 +1200

    Refactor Identity: switch to AddIdentity for role support, add lockout, Session, Razor Pages, fix EmailSender

--- VibeWave/Program.cs
+++ VibeWave/Program.cs
@@ -4,8 +4,9 @@ using VibeWave.DataAccess.Repository;
 using VibeWave.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using VibeWave.Utilities;
-
 using Stripe;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,17 +14,44 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-builder.Services.AddDefaultIdentity<IdentityUser>(options=>
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options=>
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options =>
 {
     options.Password.RequireDigit=true;
     options.Password.RequiredLength = 6;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireLowercase = false;
+    //lock account
+    options.Lockout.DefaultLockoutTimeSpan=TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
 })
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.LogoutPath = "/Identity/Account/Logout";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
+//shopping cart data tempoarly saved in Session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
 
 var app = builder.Build();
 
@@ -45,7 +73,9 @@ app.UseRouting();
 app.UseAuthentication(); 
 
 app.UseAuthorization();
+app.UseSession();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
