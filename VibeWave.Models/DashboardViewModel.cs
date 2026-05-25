1e30d65dc5d6def136bc01e404c23d0803dc57ea
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-05-25 18:35:31 +1200

    Add Admin Dashboard with statistics, fix Calendar routing, and resolve model conflicts

---
+++ VibeWave.Models/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeWave.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalConcerts { get; set; }
+        public int TotalCategories { get; set; }
+        public int TotalBookings { get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalMessages { get; set; }
+    }
+}
---
+++ VibeWave/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VibeWave.DataAccess.Repository.IRepository;
+using VibeWave.Models;
+using VibeWave.Data;
+
+namespace VibeWave.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DashboardController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new DashboardViewModel
+            {
+                TotalConcerts = _unitOfWork.Concert.GetAll().Count(),
+                TotalCategories = _unitOfWork.Category.GetAll().Count(),
+                TotalBookings = _unitOfWork.Booking.GetAll().Count(),
+                TotalUsers = _userManager.Users.Count(),
+                TotalMessages = _unitOfWork.ContactMessage.GetAll().Count()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
